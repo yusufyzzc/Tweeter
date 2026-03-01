ba7ace761779e86e0a2ea1996a749de6270d13e6
Yusuf Yazici <yusufyzzc1@gmail.com>
2026-03-01 23:00:12 +0100

    Add deployment configuration files and production settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangotweet.wsgi --log-file -
--- djangotweet/settings.py
+++ djangotweet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j-qcrv^^uer#o56i+t2f+x@*&s7apf(lp!+ogs!(@ukn08_3$4'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-j-qcrv^^uer#o56i+t2f+x@*&s7apf(lp!+ogs!(@ukn08_3$4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
@@ -116,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'tweeter:list_tweets' 
 LOGOUT_REDIRECT_URL = '/'
